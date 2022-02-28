1113f8b385ee3abb30dacf11bc16df996a69c493
EvaSilMin <91351612+EvaSilMin@users.noreply.github.com>
2022-02-28 16:05:07 -0600

    Update numvotos.php

--- Actividad6/numvotos.php
+++ Actividad6/numvotos.php
@@ -1,4 +1,5 @@
 
+
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
@@ -41,19 +42,17 @@
                 if(mysqli_num_rows($result)>0)
                 {
                   while($row= mysqli_fetch_assoc($result)){
-                    //if($row['voto_por'])
                       $mc++;
                   }
-                  //$partidoid=$_REQUEST['voto'];
+          
                   $id_partido= 6;
 
                   //SE LLENA LA BARRA DE VOTOS
 
-                  echo "<strong>movimiento ciudadano</strong><br>";// APARECE NOMBRE DEL PARTIDO
                   echo "
                   <div class='progress'>
                     <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow=\"$id_partido\" aria-valuemin=\"0\" aria-valuemax=\"100\" style='width: ".$id_partido."%'>
-                      <span class='sr-only'>BJP</span>
+                      <span class='sr-only'>MC</span>
                     </div>
                   </div>
                   ";
@@ -67,19 +66,16 @@
                 {
                   while($row= mysqli_fetch_assoc($result))
                   {
-                    //if($row['voto_por'])
                       $morena++;
                   }
 
 
-                 //$partidoid=$_REQUEST['voto'];
                  $id_partido= 7;
 
-                  echo "<strong>Partido MORENA</strong><br>";// APARECE NOMBRE DEL PARTIDO
                   echo "
                   <div class='progress'>
                     <div class='progress-bar progress-bar-primary' role='progressbar' aria-valuenow=\"70\" aria-valuemin=\"0\" aria-valuemax=\"100\" style='width: ".$id_partido."%'>
-                      <span class='sr-only'>BJP</span>
+                      <span class='sr-only'>MORENA</span>
                     </div>
                   </div>
                   ";
@@ -93,19 +89,16 @@
                 {
                   while($row= mysqli_fetch_assoc($result))
                   {
-                    //if($row['voto_por'])
+               
                       $pan++;
                   }
 
-
-                  //$partidoid=$_REQUEST['voto'];
                   $id_partido= 1;
 
-                  echo "<strong>Partido PAN</strong><br>";// APARECE NOMBRE DEL PARTIDO
                   echo "
                   <div class='progress'>
                     <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow=\"70\" aria-valuemin=\"0\" aria-valuemax=\"100\" style='width: ".$id_partido."%'>
-                      <span class='sr-only'>BJP</span>
+                      <span class='sr-only'>PAN</span>
                     </div>
                   </div>
                   ";
@@ -119,19 +112,16 @@
                 {
                   while($row= mysqli_fetch_assoc($result))
                   {
-                    //if($row['voto_por']) 
                       $prd++;
                   }
 
-
-                  //$partidoid=$_REQUEST['voto'];
                   $id_partido= 3;
 
-                  echo "<strong>Partido PRD</strong><br>";// APARECE NOMBRE DEL PARTIDO
+              
                   echo "
                   <div class='progress'>
                     <div class='progress-bar progress-bar-warning' role='progressbar' aria-valuenow=\"70\" aria-valuemin=\"0\" aria-valuemax=\"100\" style='width: ".$id_partido."%'>
-                      <span class='sr-only'>TMC</span>
+                      <span class='sr-only'>PRD</span>
                     </div>
                   </div>
                   ";
@@ -146,19 +136,16 @@
                 {
                   while($row= mysqli_fetch_assoc($result))
                   {
-                    //if($row['voto_por'])
                       $pri++;
                   }
 
-
-                  //$partidoid=$_REQUEST['voto'];
                   $id_partido= 2;
 
-                  echo "<strong>Partido PRI</strong><br>";// APARECE NOMBRE DEL PARTIDO
+       
                   echo "
                   <div class='progress'>
                     <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow=\"70\" aria-valuemin=\"0\" aria-valuemax=\"100\" style='width: ".$id_partido."%'>
-                      <span class='sr-only'>BJP</span>
+                      <span class='sr-only'>PRI</span>
                     </div>
                   </div>
                   ";
@@ -172,19 +159,15 @@
                 {
                   while($row= mysqli_fetch_assoc($result))
                   {
-                    //if($row['voto_por'])
                       $pt++;
                   }
 
-
-                 // $partidoid=$_REQUEST['voto'];
                   $id_partido= 4;
 
-                  echo "<strong>Partido PT</strong><br>";// APARECE NOMBRE DEL PARTIDO
                   echo "
                   <div class='progress'>
                     <div class='progress-bar progress-bar-warning' role='progressbar' aria-valuenow=\"70\" aria-valuemin=\"0\" aria-valuemax=\"100\" style='width: ".$id_partido."%'>
-                      <span class='sr-only'>TMC</span>
+                      <span class='sr-only'>PT</span>
                     </div>
                   </div>
                   ";
@@ -211,7 +194,7 @@
                   echo "
                   <div class='progress'>
                     <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow=\"70\" aria-valuemin=\"0\" aria-valuemax=\"100\" style='width: ".$id_partido."%'>
-                      <span class='sr-only'>BJP</span>
+                      <span class='sr-only'>PVE</span>
                     </div>
                   </div>
                   ";
